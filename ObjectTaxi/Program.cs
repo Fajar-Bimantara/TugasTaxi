4ce9e692866123fd23e32af3d4954f84d00d112b
PC <pfajarpca@gmail.com>
2021-03-16 19:47:33 +0700

    TugasTaxi

---
+++ ObjectTaxi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object Taxi
+            Taxi taxi = new Taxi();
+
+            taxi.NamaPengemudi = "Kamidi";
+            taxi.MembawaPenumpang = "Ya";
+            taxi.NomerPenumpang = 10;
+
+            // pemanggil method
+            taxi.InfoDriver();
+            taxi.InfoTaxi();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ObjectTaxi/Taxi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTaxi
+{
+    public class Taxi
+    {
+        // propertis
+        public string NamaPengemudi { get; set; }
+        public string MembawaPenumpang { get; set; }
+        public float NomerPenumpang { get; set; }
+
+        public void InfoDriver()
+        {
+            Console.WriteLine("Nama Pengemudi: {0}", NamaPengemudi);
+            Console.WriteLine("Membawa Penumpang : {0}", MembawaPenumpang);
+            Console.WriteLine("Nomer Penumpang : {0}", NomerPenumpang);
+            
+        }
+
+        public void InfoTaxi()
+        {
+            Console.WriteLine("Informasi Taxi :");
+            Console.WriteLine("{0} Sedang Menjemput Penumpang\n", NamaPengemudi);
+            Console.WriteLine("{0} Sedang Mengantar Penumpang\n", NamaPengemudi);
+        }
+    }
+
+}
